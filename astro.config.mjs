da718ac955437a9b2506753591aee2942f22bbfc
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-08 07:41:58 +0900

    Add: scssの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,14 @@ export default defineConfig({
     // 例: ビルドの間に `page/index.html` のかわりに `page.html` を生成します。
     format: "file",
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // importしたいファイルを記載
+          additionalData: `@import "src/styles/_mixin.scss";`,
+        },
+      },
+    },
+  },
 });
